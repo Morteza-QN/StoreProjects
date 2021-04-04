3689187b81380ecfe947e9ca5988d8e43e985e4f
morteza <mortezaghorbani3@gmail.com>
2021-04-04 15:21:42 +0430

    complete navigation component

--- app/src/main/java/com/morteza/storeproject/feature/main/MainActivity.kt
+++ app/src/main/java/com/morteza/storeproject/feature/main/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.morteza.storeproject.feature.main
 
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import com.example.android.navigationadvancedsample.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.morteza.storeproject.R
 import com.morteza.storeproject.common.NikeActivity
 
 class MainActivity : NikeActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        } // Else, need to wait for onRestoreInstanceState
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationMain)
+
+        val navGraphIds = listOf(R.navigation.home, R.navigation.cart, R.navigation.profile)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+
+        /*  // Whenever the selected controller changes, setup the action bar.
+          controller.observe(this, Observer { navController ->
+              setupActionBarWithNavController(navController)
+          })*/
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
---
+++ app/src/main/java/com/morteza/storeproject/feature/profile/ProfileFragment.kt
@@ -0,0 +1,20 @@
+package com.morteza.storeproject.feature.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.morteza.storeproject.R
+import com.morteza.storeproject.common.NikeFragment
+
+class ProfileFragment : NikeFragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
