3140ee9a3a787b524099bfc0758878e4ec1e91da
morteza <mortezaghorbani3@gmail.com>
2021-07-26 12:39:57 +0430

    step 2 detail

--- app/src/main/java/com/morteza/storeproject/feature/product/ProductDetailsActivity.kt
+++ app/src/main/java/com/morteza/storeproject/feature/product/ProductDetailsActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.morteza.storeproject.R
 import com.morteza.storeproject.common.formatPriceToman
 import com.morteza.storeproject.services.imageLoading.ImageLoadingService
+import com.morteza.storeproject.view.scroll.ObservableScrollViewCallbacks
+import com.morteza.storeproject.view.scroll.ScrollState
 import kotlinx.android.synthetic.main.activity_product_details.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -23,8 +25,21 @@ class ProductDetailsActivity : AppCompatActivity() {
 			productDetailTitleTv.text = it.title
 			previousPriceDetailTv.text = formatPriceToman(it.previous_price)
 			priceDetailTv.text = formatPriceToman(it.price)
-
+			toolbarTitleTv.text = it.title
 		}
+		productIv.post {
+			val productIvHeight = productIv.height
+			val toolbar = toolbarView
+			val image = productIv
+			observableScrollView.addScrollViewCallbacks(object : ObservableScrollViewCallbacks {
+				override fun onScrollChanged(scrollY: Int, firstScroll: Boolean, dragging: Boolean) {
+					toolbar.alpha = scrollY.toFloat() / productIvHeight.toFloat()
+					image.translationY = scrollY.toFloat() / 2
+				}
 
+				override fun onDownMotionEvent() {}
+				override fun onUpOrCancelMotionEvent(scrollState: ScrollState?) {}
+			})
+		}
 	}
 }
